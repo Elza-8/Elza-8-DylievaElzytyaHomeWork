d0e5a8495b928f26c34d7cdaab39a17b084219bc
Elza-8 <dulieva-xxl@mail.ru>
2022-11-27 23:42:49 +0500

    Dz_Seminar_7

---
+++ Dz_Seminar_7/Program.cs
@@ -0,0 +1,178 @@
+﻿int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, columns];
+    
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < columns; j++)
+            array[i,j] = new Random().Next(minValue, maxValue + 1);
+
+    return array;
+}
+
+
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+
+    Console.WriteLine();
+    }
+    Console.WriteLine();  
+}
+
+
+// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+/*
+double[,] CreateDoubleRandom2dArray(int rows, int columns, int minValue, int maxValue)
+{
+    double[,] array = new double[rows, columns];
+    
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < columns; j++)
+        {
+            array[i,j] = minValue + (new Random().NextDouble()) * (maxValue - minValue);
+            array[i,j] = Math.Round(array[i,j], 2);
+        }
+
+    return array;
+}
+
+void Show2dArray(double[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+
+    Console.WriteLine();
+    }
+    Console.WriteLine();  
+}
+
+Console.Write("Input a number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+double[,] myArray = CreateDoubleRandom2dArray(m, n, min, max);
+
+Show2dArray(myArray);
+*/
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 1 7 -> числа с такими индексами в массиве нет
+
+/*
+void IndexArray(int[,] array, int a, int b)
+{   
+    if(a < array.GetLength(0) && b < array.GetLength(1))
+    {
+        Console.WriteLine($"Under the given coordinates , the number {array[a,b]}");
+        Console.WriteLine(); 
+    }
+
+    else Console.WriteLine("Empty under the specified coordinates");
+    Console.WriteLine(); 
+}
+
+
+Console.Write("Input a number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the horizontal coordinate: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the horizontal vertically: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(m, n, min, max);
+Show2dArray(myArray);
+IndexArray(myArray, a, b);
+*/
+
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+double[] ArithmeticMean(int[,] array)
+{
+    int size = array.GetLength(1);
+    double[] doubleArray = new double[size];
+    int i2 = 0;
+
+    for(int j = 0; j < array.GetLength(1); j++)
+    {
+        for(int i = 0; i < array.GetLength(0); i++)
+        {
+            doubleArray[i2] = doubleArray[i2] + array[i,j];
+        }
+        doubleArray[i2] = doubleArray[i2] / array.GetLength(0);
+        i2++;
+        
+    }
+    return doubleArray;
+}
+
+void PrintDoubleArray(double[] array)
+{
+    Console.Write("The created array is: ");
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+Console.Write("Input a number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(m, n, min, max);
+Show2dArray(myArray);
+
+
+double[] arr = ArithmeticMean(myArray);
+PrintDoubleArray(arr);
+
+
+
+
+
